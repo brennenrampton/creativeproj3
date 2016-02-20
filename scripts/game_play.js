8282b45d518550a8bdd747ff934407ed32b34846
brennenrampton <rampton.brennen@gmail.com>
2016-02-20 16:57:56 -0700

    added lasers

--- scripts/game_play.js
+++ scripts/game_play.js
@@ -95,7 +95,7 @@ var Game = {
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
 
-		game.physics.arcade.overlap(lasers, tags, collisionHandler(null, this));
+		game.physics.arcade.overlap(lasers, tags, collisionHandler);
 
 		if (cursors.left.isDown) {
 			player.body.velocity.x = -250;
