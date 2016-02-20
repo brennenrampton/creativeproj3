a1fda5f9337a4a20bd1bedafcc2bb5574a280ede
brennenrampton <rampton.brennen@gmail.com>
2016-02-20 15:50:54 -0700

    added angular things

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+var name, time;
+
+angular.module('News', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('game', {
+      url: '/game',
+      templateUrl: '/game.html',
+      controller: 'GameCtrl'
+    })
+    .state('done', {
+      url: '/done',
+      templateUrl: '/done.html',
+      controller: 'DoneCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]) 
+
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+	$scope.saveName = function(input) {
+		name = input;
+	}
+}])
+
+.controller('GameCtrl', [
+'$scope',
+function($scope){
+	$scope.saveScore = function(){
+
+	}
+}]);
+
+.controller('DoneCtrl', [
+'$scope',
+function($scope){
+  $scope.person = name;
+
+  
+}]);
