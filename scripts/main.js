6fe5e9921b7b6adbdeb817e380c618ddb7e4add6
brennenrampton <rampton.brennen@gmail.com>
2016-02-20 14:36:01 -0700

    things

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var game;
 
 game = new Phaser.Game(600, 450, Phaser.AUTO, '');
 
-game.state.add('Menu', Menu);
+//game.state.add('Menu', Menu);
 game.state.add('Game', Game);
-game.state.start('Menu');
-game.state.add('Game_Over', Game_Over);
+game.state.start('Game');
+//game.state.add('Game_Over', Game_Over);
