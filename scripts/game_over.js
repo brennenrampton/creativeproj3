fa29acdc2ed103dbfec2f06d6d1e66ad0531c580
brennenrampton <rampton.brennen@gmail.com>
2016-02-23 00:44:35 -0700

    THINGS WORK MOSTLY

--- scripts/game_over.js
+++ scripts/game_over.js
@@ -3,12 +3,12 @@ var Game_Over = {
 	preload : function() {
 		game.load.image('again', 'images/again.png');
 		game.stage.backgroundColor = '#FFFFFF';
-	}
+	},
 
 	create : function() {
 		this.add.button(0, 0, 'again', this.startGame, this);
 
-	}
+	},
 
 	startGame : function() {
 		this.state.start('Game');
